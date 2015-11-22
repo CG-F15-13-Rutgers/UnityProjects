98826168ddea148bf398a23c96625565a9332dc8
erinwojo13 <erinwojo13@gmail.com>
2015-11-22 16:13:28 -0500

    Daniel in environment

---
+++ BAssignments/B2/Assets/Scripts/MovingDevilsScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingDevilsScript : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.position = new Vector3 (Mathf.PingPong (Time.time, 1)+15, transform.position.y, transform.position.z);
+	}
+}
---
+++ BAssignments/B2/Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleController : MonoBehaviour {
+	//private Rigidbody rb;
+	//public float speed;
+
+	void Start(){
+		//rb=GetComponent<Rigidbody>();
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		//float moveHorizontal = Input.GetAxis ("Horizontal");
+		//float moveVertical = Input.GetAxis ("Vertical");
+		//Vector3 movement= new Vector3(moveHorizontal, 0.0f, moveVertical);
+		//rb.AddForce(movement*speed);
+		//transform.Translate (1f*Time.deltaTime, 0f,0f);
+		transform.Translate (Input.GetAxis("Horizontal")*Time.deltaTime,0.0f, Input.GetAxis("Vertical")*Time.deltaTime);
+		print (Input.GetAxis("Horizontal"));
+	}
+}
